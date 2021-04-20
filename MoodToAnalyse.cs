cf870cf3ad188ff0b248b413936f78346fb3242b
PriyanshuSingh-5 <singhprin05@gmail.com>
2021-04-20 14:07:00 +0530

    [Priyanshu]modified ability to analyse mood

---
+++ MoodToAnalyse.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyser
+{
+    public class MoodToAnalyse
+    {
+        string message;
+        public MoodToAnalyse(string message)
+        {
+            this.message = message;
+
+        }
+
+        public string Analyser()
+        {
+            if(this.message.ToLower().Contains("happy"))
+            {
+                return "happy";
+            }
+            else
+            {
+                return "sad";
+            }
+        }
+    }
+}
